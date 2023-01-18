f20da8d80b6c9d52b447daa0bbbbb8a1a9367bc1
feindreh <119032218+feindreh@users.noreply.github.com>
2023-01-19 00:17:57 +0100

    change gameboard to 2d array

--- script.js
+++ script.js
@@ -1,30 +1,34 @@
 /* eslint-disable no-restricted-syntax */
 /* eslint-disable no-underscore-dangle */
 const Game = (() => {
-  const _gameboard = [];
+  const _gameboard = ['', 'X', 'O', 'X', '', '', '', '', ''];
   const _field = [...document.querySelectorAll('.board-field')];
+
   for (const _part of _field) {
-    _part.addEventListener('click', () => { console.log(_part.id); });
+    _part.addEventListener('click', () => {
+    });
   }
+
   const getField = () => Array.from(_field);
   const getBoard = () => Array.from(_gameboard);
   const newBoard = () => {
-    _gameboard.splice(0, _gameboard.length);
-    for (let i = 0; i < 3; i++) {
-      const block = [];
-      for (let j = 0; j < 3; j++) {
-        block.push('X');
-      }
-      _gameboard.push(block);
+    for (let i = 0; i < _gameboard.length; i++) {
+      _gameboard[i] = '';
     }
   };
   const renderBoard = () => {
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        console.log(_gameboard[i][j]);
+        console.log(_field[i][j]);
+      }
+    }
   };
 
   return {
     getField,
+    renderBoard,
     getBoard,
     newBoard,
-    renderBoard,
   };
 })();
