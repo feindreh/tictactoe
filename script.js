2fe3412e82cad3a4ac5564f99879997b488885ff
feindreh <119032218+feindreh@users.noreply.github.com>
2023-01-19 05:43:40 +0100

    querySelectors out of functions and moduls

--- script.js
+++ script.js
@@ -1,41 +1,39 @@
+/* eslint-disable no-plusplus */
+/* eslint-disable no-use-before-define */
 /* eslint-disable no-restricted-syntax */
 /* eslint-disable no-underscore-dangle */
+const createGame = document.querySelector('#createGame');
+const blackbox = document.querySelector('#blackbox');
+const prompt = document.querySelector('#prompt');
+const boardField = [...document.querySelectorAll('.board-field')];
+const text = [...document.querySelectorAll('.field-text')];
+const player1 = document.querySelector('#player1');
+const player2 = document.querySelector('#player2');
+
 const Game = (() => {
   const _gameboard = ['', '', '', '', '', '', '', '', ''];
-  const _field = [...document.querySelectorAll('.board-field')];
-  const _text = [...document.querySelectorAll('.field-text')];
-
   let _player1 = true;
-
   const _fill = (choice) => {
     if (_gameboard[choice] !== '') { return; }
     if (_player1) { _gameboard[choice] = 'X'; } else { _gameboard[choice] = 'O'; }
     _changePlayer(_player1);
   };
   const _renderBoard = () => {
-    for (let i = 0; i < _field.length; i++) {
-      _text[i].innerHTML = _gameboard[i];
+    for (let i = 0; i < boardField.length; i++) {
+      text[i].innerHTML = _gameboard[i];
     }
   };
   const _changePlayer = (boolean) => {
-    console.log(boolean);
-    const player1 = document.querySelector('#player1');
-    const player2 = document.querySelector('#player2');
-    if (boolean) { console.log('yes'); player2.style.border = 'solid 5px green'; player1.style.border = 'solid 5px white'; }
+    if (boolean) { player2.style.border = 'solid 5px green'; player1.style.border = 'solid 5px white'; }
     if (boolean !== true) { player1.style.border = 'solid 5px green'; player2.style.border = 'solid 5px white'; }
     _player1 = (boolean !== true);
   };
-
   const resetBoard = () => {
     for (let i = 0; i < _gameboard.length; i++) {
       _gameboard[i] = '';
     }
     _player1 = true;
   };
-
-  for (let i = 0; i < _field.length; i++) {
-    _field[i].addEventListener('click', () => { _fill(i); _renderBoard(); _validate(); });
-  }
   const _validate = () => {
     if (_gameboard[0] === _gameboard[1] && _gameboard[0] === _gameboard[2]) { if (_gameboard[0] !== '' && _gameboard[1] !== '' && _gameboard[2] !== '') { alert('win'); } }
     if (_gameboard[3] === _gameboard[4] && _gameboard[3] === _gameboard[5]) { if (_gameboard[3] !== '' && _gameboard[4] !== '' && _gameboard[5] !== '') { alert('win'); } }
@@ -50,20 +48,20 @@ const Game = (() => {
     }
     alert('draw');
   };
+  for (let i = 0; i < boardField.length; i++) {
+    boardField[i].addEventListener('click', () => { _fill(i); _renderBoard(); _validate(); });
+  }
   return {
     resetBoard,
   };
 })();
 
 const Prompt = (() => {
-  const _createGame = document.querySelector('#createGame');
-  _createGame.addEventListener('click', () => {
+  createGame.addEventListener('click', () => {
     console.log(Player1.name(), Player2.name());
     _hidePrompt();
   });
   const _hidePrompt = () => {
-    const blackbox = document.querySelector('#blackbox');
-    const prompt = document.querySelector('#prompt');
     blackbox.style.visibility = 'hidden';
     prompt.style.visibility = 'hidden';
   };
@@ -71,7 +69,8 @@ const Prompt = (() => {
 
 const personFactory = (selector) => {
   const name = () => selector.value;
-  return { name };
+
+  return { name, selector };
 };
 
 const Player1 = personFactory(document.querySelector('#Player1'));
