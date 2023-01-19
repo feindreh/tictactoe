ea0c5e35061cb42f2a95c753ca35ffeb16c3f565
feindreh <119032218+feindreh@users.noreply.github.com>
2023-01-19 01:32:56 +0100

    win and draw alert

--- script.js
+++ script.js
@@ -9,22 +9,34 @@ const Game = (() => {
     if (_gameboard[choice] !== '') { return; }
     if (_player1) { _gameboard[choice] = 'X'; _player1 = false; } else { _gameboard[choice] = 'O'; _player1 = true; }
   };
-  for (let i = 0; i < _field.length; i++) {
-    _field[i].addEventListener('click', () => { _fill(i); renderBoard(); });
-  }
-
-  const resetBoard = () => {
-    for (let i = 0; i < _gameboard.length; i++) {
-      _gameboard[i] = '';
-    }
-  };
-  const renderBoard = () => {
+  const _renderBoard = () => {
     for (let i = 0; i < _field.length; i++) {
       _text[i].innerHTML = _gameboard[i];
     }
   };
-
+  const _resetBoard = () => {
+    for (let i = 0; i < _gameboard.length; i++) {
+      _gameboard[i] = '';
+    }
+  };
+  for (let i = 0; i < _field.length; i++) {
+    _field[i].addEventListener('click', () => { _fill(i); _renderBoard(); validate(); });
+  }
+  const validate = () => {
+    if (_gameboard[0] === _gameboard[1] && _gameboard[0] === _gameboard[2]) { if (_gameboard[0] !== '' && _gameboard[1] !== '' && _gameboard[2] !== '') { alert('win'); } }
+    if (_gameboard[3] === _gameboard[4] && _gameboard[3] === _gameboard[5]) { if (_gameboard[3] !== '' && _gameboard[4] !== '' && _gameboard[5] !== '') { alert('win'); } }
+    if (_gameboard[6] === _gameboard[7] && _gameboard[6] === _gameboard[8]) { if (_gameboard[6] !== '' && _gameboard[7] !== '' && _gameboard[8] !== '') { alert('win'); } }
+    if (_gameboard[0] === _gameboard[3] && _gameboard[0] === _gameboard[6]) { if (_gameboard[0] !== '' && _gameboard[3] !== '' && _gameboard[6] !== '') { alert('win'); } }
+    if (_gameboard[1] === _gameboard[4] && _gameboard[1] === _gameboard[7]) { if (_gameboard[1] !== '' && _gameboard[4] !== '' && _gameboard[7] !== '') { alert('win'); } }
+    if (_gameboard[2] === _gameboard[5] && _gameboard[2] === _gameboard[8]) { if (_gameboard[2] !== '' && _gameboard[5] !== '' && _gameboard[8] !== '') { alert('win'); } }
+    if (_gameboard[0] === _gameboard[4] && _gameboard[0] === _gameboard[8]) { if (_gameboard[0] !== '' && _gameboard[4] !== '' && _gameboard[8] !== '') { alert('win'); } }
+    if (_gameboard[6] === _gameboard[4] && _gameboard[6] === _gameboard[2]) { if (_gameboard[6] !== '' && _gameboard[4] !== '' && _gameboard[2] !== '') { alert('win'); } }
+    for (const part of _gameboard) {
+      if (part === '') { return; }
+    }
+    alert('draw');
+  };
   return {
-    resetBoard,
+    validate,
   };
 })();
